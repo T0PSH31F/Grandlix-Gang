4f93316b064bdcd0c32c221130579ba13f2fd086
T0PSH31F <wrighterik77@gmail.com>
2026-05-08 20:15:11 -0700

    hardware: inject standard intel 300-series modules for luffy

--- machines/luffy/hardware.nix
+++ machines/luffy/hardware.nix
@@ -6,6 +6,10 @@
     ./disko.nix
   ];
 
+  # Modules for Intel i7 9700F / Gigabyte B365M
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-intel" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
