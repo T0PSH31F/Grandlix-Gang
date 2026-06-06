d7a1bc2709d65f4bb303e2f4e629a39d8c6429a4
/home/t0psh31f/.config/sops-nix/secrets/git_name </home/t0psh31f/.config/sops-nix/secrets/git_email>
2026-06-06 11:57:27 -0700

    key(luffy): add host public key luffy@agentaflow.com to authorized keys

--- layers/10-system/11-foundation/base.nix
+++ layers/10-system/11-foundation/base.nix
@@ -112,6 +112,7 @@
     hashedPasswordFile = lib.mkForce null;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519AAAAC3NzaC1lZDI1NTE5AAAAIJrQr8qxQTw45PNpsDNahVE23tpV3Zap+IKr6eVkL75Z t0psh31f@grandlix.gang"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg4e32XqA2CyYsHyl+urGN1Soiz00DLgc+dkDw/uFCw luffy@agentaflow.com"
     ];
   };
 }
